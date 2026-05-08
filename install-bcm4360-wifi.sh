130d6048cf5a91ad2d1c65ea5a5b002e79164cd7
aaronedward <aaron.simonson491@gmail.com>
2026-05-07 21:28:52 -0400

    cancel run on error

--- install-bcm4360-wifi.sh
+++ install-bcm4360-wifi.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+set -e
+
 # Ensure script is run as root
 if [ "$EUID" -ne 0 ]; then 
   echo "Please run as root (sudo ./install_wifi.sh)"
-  exit
+  exit 1
 fi
 
 echo "Updating package lists..."
